dd37a8eeb7f093997f98e133217155ef5f25a7bf
Stephen0wen <stevejowen@gmail.com>
2024-04-15 12:31:01 +0100

    Completes ticket #3

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -4,7 +4,7 @@ const app = require("../app");
 const request = require("supertest");
 const db = require("../db/connection");
 const { describe, test } = require("@jest/globals");
-const { response } = require("express");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => {
     return seed(data);
@@ -12,6 +12,17 @@ beforeEach(() => {
 
 afterAll(() => {
     return db.end();
+});
+
+describe("/api", () => {
+    test("GET:200 Should send an object describing all the available endpoints to the client", () => {
+        return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body }) => {
+                expect(body).toEqual(endpoints);
+            });
+    });
 });
 
 describe("api/topics", () => {
