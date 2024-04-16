40f1ac04c3b3d2720a33f1a546a6480446462368
Stephen0wen <stevejowen@gmail.com>
2024-04-16 17:10:03 +0100

    Completes happy path

--- Routes/api.router.js
+++ Routes/api.router.js
@@ -1,11 +1,13 @@
 const apiRouter = require("express").Router();
 const { getApi, getTopics } = require("../Controllers/api.controller");
-const articlesRouter = require("./articles.router");
+const { articlesRouter, commentsRouter } = require("./index");
 
 apiRouter.route("/").get(getApi);
 
 apiRouter.route("/topics").get(getTopics);
 
 apiRouter.use("/articles", articlesRouter);
+
+apiRouter.use("/comments", commentsRouter);
 
 module.exports = apiRouter;
---
+++ Routes/index.js
@@ -0,0 +1,5 @@
+const apiRouter = require("./api.router");
+const articlesRouter = require("./articles.router");
+const commentsRouter = require("./comments.router");
+
+module.exports = { apiRouter, articlesRouter, commentsRouter };
