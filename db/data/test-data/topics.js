c69ec9b64b4f569ef59ff3ccc484e060a3eb26e3
Stephen0wen <stevejowen@gmail.com>
2024-06-07 09:45:54 +0100

    Prevents code 500 when deleting a topic with no associated articles

--- db/data/test-data/topics.js
+++ db/data/test-data/topics.js
@@ -1,14 +1,18 @@
 module.exports = [
-  {
-    description: 'The man, the Mitch, the legend',
-    slug: 'mitch'
-  },
-  {
-    description: 'Not dogs',
-    slug: 'cats'
-  },
-  {
-    description: 'what books are made of',
-    slug: 'paper'
-  }
+    {
+        description: "The man, the Mitch, the legend",
+        slug: "mitch",
+    },
+    {
+        description: "Not dogs",
+        slug: "Cats",
+    },
+    {
+        description: "what books are made of",
+        slug: "paper",
+    },
+    {
+        description: "Stuff you listen to",
+        slug: "Music",
+    },
 ];
